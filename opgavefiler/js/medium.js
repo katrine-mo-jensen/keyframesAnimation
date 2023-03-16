43f0e6df108d0fd944884183592643ca914267ed
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-03-16 15:12:27 +0100

    Almost

--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -1,22 +1,38 @@
 /* Opgave 1*/
 
-let myBall=document.getElementById('ballOne');
-let myBuhButton=document.getElementById('buhOneButton');
+let myBall = document.getElementById("ballOne");
+let myBuhButton = document.getElementById("buhOneButton");
 
-
-
+myBuhButton.addEventListener("click", () => {
+  myBall.classList.toggle("animateRight");
+});
 
 /*opgave 2*/
 
-let myBalls=document.getElementsByClassName('balls');
-let myBuhTwoButton=document.getElementById('buhTwoButton');
+let myBalls = document.getElementsByClassName("balls");
+let myBuhTwoButton = document.getElementById("buhTwoButton");
 
-
+myBuhTwoButton.addEventListener("click", () => {
+  for (const myBall of myBalls) {
+    myBall.classList.toggle("animateRight");
+  }
+});
 
 /*opgave 3*/
 
+const myHardBalls = document.getElementsByClassName("hardBalls");
+let mybuhThreeButton = document.getElementById("buhThreeButton");
 
+mybuhThreeButton.addEventListener("click", () => {
+  let direction = "right";
 
-
-
-
+  for (const OneSingleBalls of myHardBalls) {
+    if (direction == "right") {
+      OneSingleBalls.classList.toggle("animateRight");
+      direction = "left";
+    } else {
+      OneSingleBalls.classList.toggle("animateLeft");
+      direction = "right";
+    }
+  }
+});
